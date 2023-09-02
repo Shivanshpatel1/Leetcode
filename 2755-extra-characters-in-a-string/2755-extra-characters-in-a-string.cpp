5d2e40b3a68d8f1316c120a7d64ab45a797e3447
Shivansh Patel <106702101+Shivanshpatel1@users.noreply.github.com>
2023-09-03 03:15:25 +0530

    Time: 90 ms (89.75%), Space: 64.7 MB (76.54%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    map<string,int>m;
-    int c(int i,string &s,vector<int>&dp){
-        if(i>=s.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-
-        int ans=INT_MAX;
-        ans = 1 + c(i+1,s,dp);
-        string k="";
-        for(int j=i;j<s.size();j++){
-            k+= s[j];
-            if(m[k]){
-                ans=min(ans,c(j+1,s,dp));
-            }
+    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index)
+    {
+        if (index >= s.size()) return 0;
+        if (dp[index] != -1) return dp[index]; //use the stored results
+        
+        string currStr = "";
+        int minExtra = s.size();
+        for (int cutIdx = index; cutIdx < s.size(); cutIdx++)
+        {
+            currStr.push_back(s[cutIdx]);
+            //currStr will be a string from (index to cutIdx)
+            
+            //if string not in dictionary, we have to delete as they are extra chars
+            int currExtra = (mp.count(currStr))? 0 : currStr.size();
+            int nextExtra = solve(s, mp, dp, cutIdx + 1);
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
         }
-        return dp[i]=ans;
+        return dp[index] = minExtra;
     }
-    int minExtraChar(string s, vector<string>& d) {
-        for(auto i : d)m[i]++;
-        vector<int>dp(s.size(),-1);
-        return c(0,s,dp);
+    int minExtraChar(string s, vector<string>& dictionary) 
+    {
+        vector<int>dp(s.size(), -1);
+        unordered_map<string, int>mp;
+        for (string& word : dictionary) mp[word]++;
+        
+        int ans = solve(s, mp, dp, 0);
+        return ans;
+        
     }
 };
