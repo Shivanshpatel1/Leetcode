6c10bdcf59c52f2449f290be880b37c1debaf4c2
Shivansh Patel <106702101+Shivanshpatel1@users.noreply.github.com>
2023-11-07 17:08:17 +0530

    Time: 3184 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-select d.name as department , e1.name as employee, e1.salary as Salary
-from employee e1, Department d 
-where e1.departmentID = d.id and   3 > (
-    select count(distinct (e2.Salary))
-    from  Employee e2
-    where e2.Salary > e1.Salary and e1.DepartmentId = e2.DepartmentId)
+select d.name as department, e.name as employee, e.salary as Salary
+ from Employee e, Department d
+where e.departmentId = d.id and 3 >= (select count(distinct e1.salary) from Employee e1 where e1.salary>=e.salary and
+e1.departmentId=e.departmentId);
